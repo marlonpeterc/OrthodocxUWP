6143e38cf258f31a0a94832ded8dafb29b3079ac
marlonpeterc <marlonpeterc@gmail.com>
2019-01-08 21:12:05 +1300

    first commit

---
+++ Orthodocx/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Orthodocx.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Orthodocx
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        #region NavigationView event handlers
+        private void nvTopLevelNav_Loaded(object sender, RoutedEventArgs e)
+        {
+            // set the initial SelectedItem
+            foreach (NavigationViewItemBase item in nvTopLevelNav.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "Home_Page")
+                {
+                    nvTopLevelNav.SelectedItem = item;
+                    break;
+                }
+            }
+            contentFrame.Navigate(typeof(HomePage));
+        }
+
+        private void nvTopLevelNav_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+        }
+
+        private void nvTopLevelNav_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                contentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                TextBlock ItemContent = args.InvokedItem as TextBlock;
+                if (ItemContent != null)
+                {
+                    switch (ItemContent.Tag)
+                    {
+                        case "Nav_Home":
+                            contentFrame.Navigate(typeof(HomePage));
+                            break;
+
+                        case "Nav_Doctors":
+                            contentFrame.Navigate(typeof(DoctorsPage));
+                            break;
+
+                        case "Nav_Patients":
+                            contentFrame.Navigate(typeof(PatientsPage));
+                            break;
+
+                        case "Nav_Schedule":
+                            contentFrame.Navigate(typeof(SchedulePage));
+                            break;
+
+                        case "Nav_Prescriptions":
+                            contentFrame.Navigate(typeof(PrescriptionsPage));
+                            break;
+
+                        case "Nav_Charts":
+                            contentFrame.Navigate(typeof(ChartsPage));
+                            break;
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Orthodocx/Views/PatientsPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Orthodocx.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PatientsPage : Page
+    {
+        public PatientsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>();
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            // Instead of hard coded items, the data could be pulled 
+            // asynchronously from a database or the internet.
+            Items.Add("Jack Sparrow");
+            Items.Add("Kaj Jak");
+            Items.Add("Pak Yew");
+            Items.Add("Item 4");
+            Items.Add("Item 5");
+        }
+    }
+}
